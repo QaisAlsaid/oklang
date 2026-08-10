d5e47ed02da25fbd1725c99ab01efe13c0950447
qais <qaisalsaid4@gmail.com>
2026-08-10 07:33:28 +0300

    updated flake, and name to ok.

--- flake.nix
+++ flake.nix
@@ -2,55 +2,42 @@
   description = "OKLang";
 
   inputs = {
-    nixpkgs.url = "nixpkgs/nixos-26.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
   };
 
   outputs = { self, nixpkgs }:
-  let
-    system = "x86_64-linux";
-    pkgs = import nixpkgs {
-      inherit system;
-    };
-    clangStdenv = pkgs.clangStdenv;
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system; };
+      clangStdenv = pkgs.clangStdenv;
 
-    okc = clangStdenv.mkDerivation {
-      pname = "okc";
-      version = "0.0.0";
-      src = self;
+      ok = clangStdenv.mkDerivation {
+        pname = "ok";
+        version = "0.0.0";
+        src = self;
+        nativeBuildInputs = with pkgs; [ cmake ninja ];
+        cmakeFlags = [ "-GNinja" ];
+      };
+    in
+    {
+      packages.${system} = {
+        default = ok;
+        inherit ok;
+      };
 
-      nativeBuildInputs = with pkgs; [
-        cmake
-        ninja
-      ];
+      checks.${system} = {
+        inherit ok;
+        regressions = pkgs.runCommand "oklang-regressions" {
+          nativeBuildInputs = [ ok ];
+        } ''
+          mkdir -p $out
+          oktest-regression ${ok}/bin/ok ${self}/tests ${ok}/bin/okc_debug | tee $out/summary.txt
+        '';
+      };
 
-      cmakeFlags = [
-        "-GNinja"
-      ];
+      devShells.${system}.default = pkgs.mkShell.override { stdenv = clangStdenv; } {
+        packages = with pkgs; [ cmake ninja lldb pkg-config git ];
+      };
     };
-  in {
-    packages.${system} = {
-      default = okc;
-      inherit okc;
-    };
-
-    checks.${system} = {
-      inherit okc;
-      regressions = pkgs.runCommand "oklang-regressions" {
-        nativeBuildInputs = [ okc ];
-      } ''
-        mkdir -p $out
-        oktest-regression ${okc}/bin/okc ${self}/tests ${okc}/bin/okc_debug | tee $out/summary.txt
-      '';
-    };
-
-    devShells.${system}.default = pkgs.mkShell.override { stdenv = clangStdenv; } {
-      packages = with pkgs; [
-        cmake
-        ninja
-        lldb
-        pkg-config
-        git
-      ];
-    };
-  };
 }
+
