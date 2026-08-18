4c378406fd925477a9a4da7495dd276837fa6fed
qais <qaisalsaid4@gmail.com>
2026-08-18 22:52:26 +0300

    fixed array remove bugs.

--- src/array.h
+++ src/array.h
@@ -16,7 +16,8 @@
   void name##_deinit(name* p_array);                                                                                   \
   bool name##_grow(name* p_array, const size_type p_new_capacity);                                                     \
   bool name##_append(name* p_array, const type p_element);                                                             \
-  bool name##_append_n(name* p_array, const type* p_elements, const size_type p_elements_count);
+  bool name##_append_n(name* p_array, const type* p_elements, const size_type p_elements_count);                       \
+  bool name##_remove(name* p_array, const size_type p_first, const size_type p_last);
 
 #define ARRAY_DEFINE(name, type, size_type)                                                                            \
   void name##_init(name* p_array) {                                                                                    \
@@ -58,6 +59,25 @@
     }                                                                                                                  \
     memcpy(p_array->data + p_array->count, p_elements, p_elements_count);                                              \
     p_array->count += p_elements_count;                                                                                \
+    return true;                                                                                                       \
+  }                                                                                                                    \
+                                                                                                                       \
+  bool name##_remove(name* p_array, const size_type p_first, const size_type p_last) {                                 \
+    if (p_first >= p_array->count || p_last < p_first) {                                                               \
+      return false;                                                                                                    \
+    }                                                                                                                  \
+    const size_type count = p_last - p_first + 1;                                                                      \
+    const size_type new_capacity = p_array->capacity - count;                                                          \
+    const size_type new_count = p_array->count - count;                                                                \
+    type* ptr = NULL;                                                                                                  \
+    ptr = (type*)reallocate(ptr, sizeof(type) * p_array->capacity, sizeof(type) * new_capacity);                       \
+    if (ptr == NULL) {                                                                                                 \
+      return false;                                                                                                    \
+    }                                                                                                                  \
+    memcpy(ptr, p_array->data, p_first);                                                                               \
+    memcpy(ptr, p_array->data + p_last + 1, p_array->count - p_last);                                                  \
+    p_array->count = new_count;                                                                                        \
+    p_array->capacity = new_capacity;                                                                                  \
     return true;                                                                                                       \
   }
 
